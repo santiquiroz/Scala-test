66e487ca08539f7f3078a6d8a0a1718ef2417299
Santiago Quiroz upegui <santiqupgui@gmail.com>
2024-04-24 14:40:49 -0500

    values, variables and types

---
+++ udemy-scala-beginners/udemy-scala-beginners/src/main/scala/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,26 @@
+package part1basics
+
+object ValuesVariablesTypes extends App {
+  //val x: Int = 42
+  val x = 42
+  println(x)
+
+  //x = 2
+
+  //vals are inmutable
+  //compiler can infer types
+
+  val aString: String = "String";  val anotherString = "another"
+
+  val aBoolean: Boolean = false
+  val aCharacter: Char = 'a'
+  val aShort: Short = 461
+  var aLong: Long = 13213123L
+  var aFloat: Float = 1221132312.3F
+
+  //variables
+
+  var aVariable: Int = 4
+  aVariable = 5
+
+}
